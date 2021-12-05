5d975f99120dbd009521fb5a802673dd5bd8d1f3
Miller777 <miller777@mail.ru>
2021-12-06 02:52:35 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/provider_edit_text/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_edit_text
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('provider_edit_text'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('11111'),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text('22222'),
+          ],
+        ),
+      ),
+    );
+  }
+}
